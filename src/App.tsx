1f4444e4388834a91d32f305ad9ba91b4503f3e2
ojage <hismajestylordkratos@gmail.com>
2023-09-27 09:35:12 +0100

    made box

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Engine, Scene } from "react-babylonjs";
+import { Vector3, Color3 } from "@babylonjs/core";
+import Box from "./components/Box";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Engine antialias={true} adaptToDeviceRatio={true} canvasId="sample-canvas">
+    <Scene>
+      <arcRotateCamera
+        name="camera1"
+        alpha={Math.PI / 3}
+        beta={Math.PI / 4.5}
+        radius={9.0}
+        target={Vector3.Zero()}
+        minZ={0.001}
+        position={Vector3.Zero()}
+      />
+
+      <hemisphericLight
+        name="light1"
+        intensity={0.7}
+        direction={Vector3.Up()}
+      />
+
+      <Box
+        name="simple-box"
+        position={new Vector3(0, 0, 0)}
+        color={Color3.FromHexString("#C8F4F9")}
+      />
+    </Scene>
+  </Engine>
+);
 
 export default App;
